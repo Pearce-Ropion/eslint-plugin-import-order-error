895d71d7ce3cdfdab726440bfedc1e9fe01aa88c
Pearce <pearce@sigmacomputing.com>
2022-07-13 21:23:31 -0700

    Commit correct import syntax

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+/* === Change These Values === */
+
+const USE_MULTIPLE_NAMESPACE_GROUPS = false;
+
+// Try using 8, 9, 10 or 11
+const INTERNAL_IMPORT_GROUPS = 9;
+
+/* =========================== */
+
+const internalImports = new Array(26).fill(0).map((_, idx) => {
+  return String.fromCharCode(97 + idx);
+});
+
+const internalImportRegex = `^(${internalImports.join('|')})(\\/|$)`;
+
+const namespaceGroups = (
+  USE_MULTIPLE_NAMESPACE_GROUPS ? ['foo', 'bar'] : ['**']
+).map(name => ({
+  pattern: `@namespace/${name}`,
+  group: 'external',
+  position: 'after',
+}));
+
+module.exports = {
+  parser: '@babel/eslint-parser',
+
+  parserOptions: {
+    requireConfigFile: false,
+  },
+
+  settings: {
+    'import/internal-regex': internalImportRegex,
+  },
+
+  plugins: ['import'],
+
+  rules: {
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          ['sibling', 'index'],
+        ],
+        pathGroups: [
+          ...namespaceGroups,
+          ...internalImports.slice(0, INTERNAL_IMPORT_GROUPS).map(name => ({
+            pattern: name,
+            group: 'internal',
+            position: 'before',
+          })),
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { bar } from '@namespace/bar';
+import { foo } from '@namespace/foo';
+
+import { a } from 'a';
+
+import { b } from 'b';
+
+import { c } from 'c';
+
+import { d } from 'd';
+
+import { e } from 'e';
+
+import { f } from 'f';
+
+import { g } from 'g';
+
+import { h } from 'h';
+
+import { i } from 'i';
+
+import { j } from 'j';
+import { k } from 'k';
+import { l } from 'l';
+import { m } from 'm';
+
+import parent from '../parent';
+
+import sibling from './sibling';
